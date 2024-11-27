8947b143ab1e5aa5b8abfe32f7bc57f2266c4084
Anastasia Egorova <air-neba@yandex.ru>
2024-11-27 23:20:45 +0300

    готовая версия

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+
+
+function renderCard() {
+  initialCards.forEach(({name, link}) => {
+  const card = addCard({name, link});
+  })
+};
+
+function addCard({name, link}){
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+ 
+  placesList.append(cardElement);
+  
+};
+
+function deleteCard() {
+  const deleteButton = content.querySelectorAll('.card__delete-button');
+  deleteButton.forEach((element) => {element.addEventListener('click', function (){
+  const listItem = element.closest('.places__item');
+  listItem.remove()
+})})};
+
+renderCard();
+deleteCard();
